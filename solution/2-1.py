5fbad3ac6747d88cf7f5e45894e09058996d8fe5
shuwei <38966382+twshutech@users.noreply.github.com>
2021-06-28 21:10:49 +0800

    Create 2-1.py

---
+++ solution/2-1.py
@@ -0,0 +1,29 @@
+def startFinding(l ,t):
+  s = 0
+  endPoint = 0
+  for index, n in enumerate(l):
+    s += n
+    if s == t:
+      endPoint = index
+
+    # Find target or reach the end or greater than limit.
+    if s == t or index == len(l) - 1 or s > 250:
+      break
+  return endPoint
+
+def solution(l, t):
+  fromAndTo = [-1, -1]
+  endPoint = 0
+  for i in range(len(l)):
+    endPoint = startFinding(l[i:], t)
+    if endPoint != 0:
+      fromAndTo[1] = endPoint + i
+      fromAndTo[0] = i
+      break
+    elif endPoint == 0 and l[i] == t:
+      fromAndTo = [i, i]
+      break
+  return(fromAndTo)
+print(solution([4, 3, 10, 2, 8], 12))
+print(solution([1, 2, 3, 4], 15))
+print(solution([1, 2, 15, 3, 4, 8, 1, 1, 1, 1], 30))
