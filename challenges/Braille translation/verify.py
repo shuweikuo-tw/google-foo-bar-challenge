a773bd2453ed6d6a63e36d75e200258476a5ca20
shuwei <38966382+twshutech@users.noreply.github.com>
2021-06-28 21:08:29 +0800

    Update challenge markdown files
    Add the challenge question md files.

---
+++ challenges/Braille translation/verify.py
@@ -0,0 +1,1 @@
+pass_msg = "You survived a week in Commander Lambda's organization, and you even managed to get yourself promoted. Hooray! Henchmen still don't have the kind of security access you'll need to take down Commander Lambda, though, so you'd better keep working. Chop chop!"
